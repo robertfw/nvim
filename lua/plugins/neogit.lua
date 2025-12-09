f6dc687bd12ed2a5464ab5005a9cef2e85375123
Robert Warner <robertfrederickwarner@gmail.com>
2025-12-09 14:57:34 -0800

    <leader><tab> goto last tab instead of last buffer when in neogit status

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -8,5 +8,19 @@ return {
   cmd = "Neogit",
   keys = {
     { "<leader>gs", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
-  }
+  },
+  config = function()
+    require("neogit").setup()
+
+    -- Set up buffer-local keybinding for neogit status buffers
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "NeogitStatus",
+      callback = function(event)
+        vim.keymap.set('n', '<leader><tab>', '<Cmd>tabprevious<CR>', {
+          buffer = event.buf,
+          desc = 'Previous tab'
+        })
+      end,
+    })
+  end
 }
