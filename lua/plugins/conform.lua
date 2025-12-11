4be683fc66ebd8b08f3be628646c0a12fb18dc36
Robert Warner <robertfrederickwarner@gmail.com>
2025-12-11 12:28:23 -0800

    format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -15,6 +15,10 @@ return {
     },
     opts = {
       notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = 'fallback',
+      },
       formatters_by_ft = {
         lua = { 'stylua' },
         python = { 'ruff_fix', 'ruff_format' },
